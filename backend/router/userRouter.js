189f6162fa0f00c1dfc44ddadd19cd163e78f67a
Kaarti22 <kaartimondi@gmail.com>
2024-07-13 00:06:46 +0530

    changes

--- backend/router/userRouter.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { login, register } from "../controllers/userController.js";
-
-const router = express.Router();
-
-router.post("/register", register);
-router.post("/login", login);
-
-export default router;
