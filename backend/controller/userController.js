d259f6c4dde3c0e5fd167f886bc030caeecccba2
Kaarti22 <kaartimondi@gmail.com>
2024-07-13 14:15:49 +0530

    update profile and password

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -75,23 +75,131 @@ export const register = catchAsyncErrors(async (req, res, next) => {
 
 export const login = catchAsyncErrors(async (req, res, next) => {
   const { email, password } = req.body;
-  
+
   if (!email || !password) {
     return next(new ErrorHandler("Email and password are required!!!"));
   }
-  
+
   const user = await User.findOne({ email }).select("+password");
-  
-  if(!user){
+
+  if (!user) {
     return next(new ErrorHandler("Invalid email or password!!!"));
   }
 
   const isPasswordMatched = await user.comparePassword(password);
 
-  if(!isPasswordMatched){
+  if (!isPasswordMatched) {
     return next(new ErrorHandler("Invalid email or password!!!"));
   }
 
   generateToken(user, "Logged In", 200, res);
-
 });
+
+export const logout = catchAsyncErrors(async (req, res, next) => {
+  res
+    .status(200)
+    .cookie("token", "", {
+      expires: new Date(Date.now()),
+      httpOnly: true,
+    })
+    .json({
+      success: true,
+      message: "Logged out",
+    });
+});
+
+export const getUser = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.user._id);
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
+
+export const updateProfile = catchAsyncErrors(async (req, res, next) => {
+  const newUserData = {
+    fullName: req.body.fullName,
+    email: req.body.email,
+    phone: req.body.phone,
+    about: req.body.about,
+    portfolioURL: req.body.portfolioURL,
+    githubURL: req.body.githubURL,
+    instagramURL: req.body.instagramURL,
+    facebookURL: req.body.facebookURL,
+    twitterURL: req.body.twitterURL,
+    linkedInURL: req.body.linkedInURL,
+  };
+  if (req.files && req.files.avatar) {
+    const avatar = req.files.avatar;
+    const user = await User.findById(req.user.id);
+    const profileImageId = user.avatar.public_id;
+    await cloudinary.uploader.destroy(profileImageId);
+    const cloudinaryResponse = await cloudinary.uploader.upload(
+      avatar.tempFilePath,
+      { folder: "Avatar" }
+    );
+    newUserData.avatar = {
+      public_id: cloudinaryResponse.public_id,
+      url: cloudinaryResponse.secure_url,
+    };
+  }
+  if (req.files && req.files.resume) {
+    const resume = req.files.resume;
+    const user = await User.findById(req.user.id);
+    const resumeId = user.resume.public_id;
+    await cloudinary.uploader.destroy(resumeId);
+    const cloudinaryResponse = await cloudinary.uploader.upload(
+      resume.tempFilePath,
+      { folder: "Resume" }
+    );
+    newUserData.resume = {
+      public_id: cloudinaryResponse.public_id,
+      url: cloudinaryResponse.secure_url,
+    };
+  }
+  const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  res.status(200).json({
+    success: true,
+    message: "Profile updated",
+    user,
+  });
+});
+
+export const updatePassword = catchAsyncErrors(async (req, res, next) => {
+  const { currentPassword, newPassword, confirmNewPassword } = req.body;
+  if (!confirmNewPassword || !newPassword || !currentPassword) {
+    return next(new ErrorHandler("Please fill all the fields", 400));
+  }
+  const user = await User.findById(req.user.id).select("password");
+  const isPasswordMatched = await user.comparePassword(currentPassword);
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Incorrect current password", 400));
+  }
+  if (newPassword != confirmNewPassword) {
+    return next(
+      new ErrorHandler(
+        "New password and confirm new password do not match!!",
+        400
+      )
+    );
+  }
+  user.password = newPassword;
+  await user.save();
+  res.status(200).json({
+    success: true,
+    message: "Password updated successfully.",
+  });
+});
+
+export const getUserForPortfolio = catchAsyncErrors(async (req, res, next) => {
+  const id = "6649f9ad005284db9390587a";
+  const user = await User.findById(id);
+  res.status(200).json({
+    success: true,
+    user,
+  });
+})
--- backend/router/userRoutes.js
+++ backend/router/userRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { login, register } from "../controller/userController.js";
+import { login, register, logout, getUser, updateProfile, updatePassword, getUserForPortfolio } from "../controller/userController.js";
+import {isAuthenticated} from "../middlewares/auth.js"
 
 const router = express.Router();
 
 router.post("/register", register); 
 router.post("/login", login); 
+router.get("/logout", isAuthenticated, logout);  
+router.get("/me", isAuthenticated, getUser);
+router.put("/update/me", isAuthenticated, updateProfile);
+router.put("/update/password", isAuthenticated, updatePassword);
+router.get("/me/portfolio", getUserForPortfolio);
 
 export default router;
